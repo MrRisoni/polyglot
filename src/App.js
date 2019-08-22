d59bf7ca797803ee4ef40b563a7ce358e64c1fb4
MrRisoni <vanguelisk@gmail.com>
2019-08-22 11:20:51 +0300

    NO text

--- src/App.js
+++ src/App.js
@@ -24,10 +24,32 @@ class App extends Component {
 
   render() {
 
-    let txt = "<h1>Чем </h1> <h1>люди </h1> <h1>живы </h1> <br>";
-    txt += "<h2>I</h2> <br>";
-    txt += "Жил сапожник с женой и детьми у мужика на квартире. Ни дома своего, ни земли у <br>";
-    txt += " него не было, и кормился он с семьею сапожной работой. Хлеб был дорогой, а работа "
+    let txtRU = `<h1>Чем </h1> <h1>люди </h1> <h1>живы </h1> <br>
+    <h2>I</h2> <br>
+    Жил сапожник с женой и детьми у мужика на квартире. Ни дома своего, ни земли у <br>
+    него не было, и кормился он с семьею сапожной работой. Хлеб был дорогой, а работа`;
+
+
+    let txtNO = `<h2>​FØRSTE</h2> <h2>HANDLING</h2><br>
+Oppe i Sneen paa Fjeldvidderne. Taagen ligger tætt og tung; det er Regnvejr og halvmørkt.<br>
+Brand, (sortklædt, med Stav og Skræppe, kravler sig frem vestover).<br>
+En Bonde og hans halvvoxne Søn, (som har slaaet Følge med, er noget bagefter).<br>
+<br>
+
+<b>BONDEN</b>: raaber efter Brand <br>
+Hej, Fremmedkarl, far ej saa fort! <br>
+Hvor er du? <br>
+<b>BRAND</b>: Her! <br>
+<b>BONDEN</b>: Du gaar dig bort! <br>
+Nu tykkner Skodden slig, at knappt<br>
+en ser saa langt, som Staven rækker – <br>
+<b>SØNNEN</b>:
+Faer, her er Spriker! <br>
+<b>BONDEN</b>:
+Her er Sprækker! <br> `;
+
+ const txt = txtNO;
+
 
     const txtArray = txt.split('<br>');
     console.log('print');
