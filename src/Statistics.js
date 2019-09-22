6f3229f8d64025cc302dcce6c79f9c55ae32de8a
phoenix <vanguelisk@gmail.com>
2019-09-22 12:14:07 +0300

    ignore xml cange routes

---
+++ src/Statistics.js
@@ -0,0 +1,73 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+import {Link} from 'react-router-dom'
+
+class Statistics extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            fetched: false
+        };
+
+        this.endPoint = 'https://shielded-brook-92440.herokuapp.com';
+        //  this.endPoint =  'http://localhost:3500';
+    }
+
+    componentDidMount() {
+        const self = this;
+
+        axios.get(self.endPoint + '/api/statistics').then(rsp => {
+            self.setState({
+                data: rsp.data,
+                fetched: true
+            })
+        })
+    }
+
+    render() {
+        return (
+            <section>
+
+                {this.state.fetched &&
+                <table className="table table-bordered table-striped">
+                    <thead>
+                    <tr>
+                        <th>Language</th>
+                        <th>Total Words</th>
+                        <th>Total Avg Due</th>
+                        <th>Translated to</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {this.state.data.map((dt) => {
+                        return (<tr key={dt.lang}>
+                            <td>{dt.lang}</td>
+                            <td>{dt.totalWords}</td>
+                            <td>{dt.avgDue}</td>
+                            <td>{dt.traLangData}</td>
+                        </tr>)
+                    })}
+                    </tbody>
+                </table>
+                }
+
+                <ul>
+                    <li>
+                        <Link to="/flash">Flashcards</Link>
+                    </li>
+                    <li>
+                        <Link to="/newword">New Word</Link>
+                    </li>
+                </ul>
+
+            </section>
+
+        );
+    }
+}
+
+export default Statistics;
+
+
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import FlashCard from './FlashCard';
 import NewWord from './NewWord';
+import Statistics from './Statistics';
 
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter, Route} from "react-router-dom";
@@ -12,7 +13,8 @@ import {BrowserRouter, Route} from "react-router-dom";
 ReactDOM.render(
     <BrowserRouter>
         <div>
-            <Route exact path="/" component={FlashCard}/>
+            <Route exact path="/" component={Statistics}/>
+            <Route exact path="/flash" component={FlashCard}/>
             <Route exact path="/newword" component={NewWord}/>
         </div>
     </BrowserRouter>,
